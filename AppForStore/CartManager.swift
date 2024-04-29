259d9118c11a79a959de382ab1c402fa1ec59baf
bemadeveloper <akbermet1990@icloud.com>
2024-04-29 21:05:07 +0600

    Creating Main Functionality

---
+++ AppForStore/CartManager.swift
@@ -0,0 +1,23 @@
+//
+//  CartManager.swift
+//  AppForStore
+//
+//  Created by Bema on 29/4/24.
+//
+
+import Foundation
+
+class CartManager: ObservableObject {
+    @Published private(set) var products: [Product] = []
+    @Published private(set) var total: Int = 0
+    
+    func addToCart(product: Product) {
+        products.append(product)
+        total += product.price
+    }
+    
+    func removeFromCart(product: Product) {
+        products = products.filter { $0.id != product.id }
+        total -= product.price
+    }
+}
---
+++ AppForStore/Views/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  AppForStore
+//
+//  Created by Bema on 29/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var cartManager = CartManager()
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList, id: \.id) { product in
+                        ProductCard(product: product)
+                            .environmentObject(cartManager)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(Text("KGmart Shop"))
+            .toolbar {
+                NavigationLink {
+                    CartView()
+                        .environmentObject(cartManager)
+                } label: {
+                    CartButton(numberOfProducts: cartManager.products.count)
+                }
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
