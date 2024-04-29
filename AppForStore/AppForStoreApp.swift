3be339190dd4ca72839625fc1a496a004fcd163f
bemadeveloper <akbermet1990@icloud.com>
2024-04-29 20:54:25 +0600

    Initial Commit

---
+++ AppForStore/AppForStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppForStoreApp.swift
+//  AppForStore
+//
+//  Created by Bema on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AppForStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
